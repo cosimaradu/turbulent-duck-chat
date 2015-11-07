1d304245215a8b22f12e343d968fbf06ba8337a0
Cosima Radu <cosima.radu@sage.com>
2015-11-07 13:10:28 -0500

    db update

--- db/migrate/20151106224901_create_posts.rb
+++ db/migrate/20151106224901_create_posts.rb
@@ -1,6 +1,6 @@
 class CreatePosts < ActiveRecord::Migration
   def change
-    create_table :posts do |t|
+    create_table :posts, options: 'ROW_FORMAT=DYNAMIC' do |t|
       t.text :message
       t.string :user
       t.timestamps null: false
