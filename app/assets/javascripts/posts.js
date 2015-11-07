a76e1b3f0ec6d7649977e441cbf0c71cea739847
Cosima Radu <cosima.radu@sage.com>
2015-11-07 16:28:35 -0500

    styling

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+    var wrapper = $('.screen_wrapper');
     $(function () {
         var window_height = $(window).height(),
             content_height = window_height - 200;
@@ -8,12 +8,16 @@ $(document).ready(function() {
 
     $(window).resize(function () {
         var window_height = $(window).height(),
-            content_height = window_height - 200;
-        $('.screen_wrapper').height(content_height);
+            content_height = window_height - 300;
+        wrapper.height(content_height);
+        wrapper.stop().animate({
+            scrollTop: wrapper[0].scrollHeight
+        }, 1600);
+
     });
 
-    $('.screen_wrapper').stop().animate({
-        scrollTop: $(".screen_wrapper")[0].scrollHeight
+    wrapper.stop().animate({
+        scrollTop: wrapper[0].scrollHeight
     }, 1600);
 
 });
